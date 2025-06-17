a84a9ceddeafc08b453417bd462b3c2dc593aead
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-17 21:40:59 +0530

    practice variables

---
+++ src/com/ja/datatypes/DatatypesEx1.java
@@ -0,0 +1,17 @@
+package com.ja.datatypes;
+
+public class DatatypesEx1 {
+    static long l=6;
+
+    float f=5f;
+    public static void main(String[] args) {
+        int i=100;
+        System.out.println(i);
+
+
+        //classname ref=new classname();
+        DatatypesEx1 ex1=new DatatypesEx1();
+        System.out.println(ex1.f);
+        System.out.println(DatatypesEx1.l);
+    }
+}

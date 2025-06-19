769d69b5c6a57974680ac34a918c4b72c086bf00
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-19 23:35:54 +0530

    about bulb

---
+++ src/methods/methodex2.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class methodex2 {
+    String name = "bulb";
+     int cost = 5;
+
+    public void display(String n, int c) {
+        System.out.println("String name=" + n);
+        System.out.println("int cost=" + c);
+
+    }
+
+    public static void main(String[] args) {
+        methodex2 obj = new methodex2();
+        obj.display(obj.name, obj.cost);
+    }
+}

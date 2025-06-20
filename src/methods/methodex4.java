bc648253691361cca74f2c115a28822906121006
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-20 17:43:43 +0530

    country

---
+++ src/methods/methodex4.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class methodex4 {
+    String name = "India";
+    int num = 1122;
+
+    public void display() {
+        System.out.format("Stringname=" +name,"intname=" +num);
+    }
+
+    public static void main(String[] args) {
+        methodex4 country = new methodex4();
+        country.display();
+
+    }
+
+}
+
+
+

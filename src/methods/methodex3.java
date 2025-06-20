ae1a6f2f4cb048ec42b6df73ddc389071cc97e3e
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-20 13:42:04 +0530

    practice 2

--- src/methods/methodex3.java
+++ src/methods/methodex3.java
@@ -1,18 +1,16 @@
 package methods;
 
 public class methodex3 {
-    String name = "Bottle";
-    int num = 3;
+    float num1 = 4f;
+    int num2 = 4;
 
-    public int display(String n, int a) {
-        System.out.println("String name = " + n);
-        System.out.println("int num = "+ a);
-        return a;
+    public int add(float num1,int num2) {
+        System.out.println("float num1=" +num1 +", int num2=" +num2);
+        return 0;
     }
 
     public static void main(String[] args) {
         methodex3 obj = new methodex3();
-        obj.display("Bottle" , 3);
-
+        obj.add(4f,4);
     }
 }

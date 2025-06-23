cbce8f752dc105f3f6eadf702ec9251836a6f180
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-23 18:29:17 +0530

    arithematic

---
+++ src/com/ja/operators/arithematic.java
@@ -0,0 +1,15 @@
+package com.ja.operators;
+
+public class arithematic {
+    public static void main(String[] args) {
+
+
+        int a = 19;
+        int b = 2;
+        int sum = a + b;
+        int diff = a-b;
+        System.out.println("addition(a+b)="+ sum);
+        System.out.println("subtraction(a-b)=" + diff);
+
+    }
+}

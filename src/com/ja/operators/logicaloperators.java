61c019598d336e8be030c32adc0c8cd5598ff0b2
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-24 20:56:56 +0530

    unary

--- src/com/ja/operators/logicaloperators.java
+++ src/com/ja/operators/logicaloperators.java
@@ -1,4 +1,10 @@
 package com.ja.operators;
 
 public class logicaloperators {
+    public static void main(String[] args) {
+        int a = 00;
+        int b = 9;
+        System.out.println(a>b && b<a);
+        System.out.println(a != b );
+    }
 }
---
+++ src/com/ja/operators/unary.java
@@ -0,0 +1,10 @@
+package com.ja.operators;
+
+public class unary {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = ++a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}

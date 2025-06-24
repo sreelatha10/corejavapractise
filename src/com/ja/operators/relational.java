ba8db9e607d70646d377289f35ea72930913846b
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-24 20:42:02 +0530

    relational

---
+++ src/com/ja/operators/relational.java
@@ -0,0 +1,11 @@
+package com.ja.operators;
+
+public class relational {
+    public static void main(String[] args) {
+        int a = 7;
+       int b =9;
+        System.out.println(a == b);
+        System.out.println(a >= b);
+        System.out.println(a != b);
+    }
+}

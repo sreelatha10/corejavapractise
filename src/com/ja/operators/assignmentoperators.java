aee2d172c882e47d3cb42d9ece28ec8c7247c0e5
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-24 20:35:30 +0530

    assigned

---
+++ src/com/ja/operators/assignmentoperators.java
@@ -0,0 +1,18 @@
+package com.ja.operators;
+
+public class assignmentoperators {
+
+    public static void main(String[] args) {
+        int a = 8;
+        System.out.println("Imitial a:" + a);
+        a += 8;
+        System.out.println("after += 8: " + a);
+        a -= 4;
+        System.out.println("after -= 4: " + a);
+        a *= 9;
+        System.out.println("after *= 9: " + a);
+        a /= 7;
+        System.out.println("after /= 7: " + a);
+    }
+}
+

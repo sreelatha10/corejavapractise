80e720106cfaa2cc138cc7a1abe943aa09c64452
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-25 18:59:37 +0530

    while

---
+++ src/com/ja/loops/whileloop.java
@@ -0,0 +1,12 @@
+package com.ja.loops;
+
+public class whileloop {
+    public static void main(String[] args) {
+
+
+        for (int i = 60; i <= 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}

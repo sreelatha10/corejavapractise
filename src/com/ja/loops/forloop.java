a64429528c892bc2de450feee23ff2db556b481a
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 13:30:29 +0530

    forloop

---
+++ src/com/ja/loops/forloop.java
@@ -0,0 +1,10 @@
+package com.ja.loops;
+
+public class forloop {
+    public static void main(String[] args) {
+
+        for ( int i = 1; i <= 10; i++) {
+            System.out.println("9 * " + i + " = " + (9 * i));
+        }
+    }
+}

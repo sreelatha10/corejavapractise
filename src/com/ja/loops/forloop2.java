8375e89de4a197d177f63f5fff63b2b0c67fe5c2
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 13:35:52 +0530

    exam2

---
+++ src/com/ja/loops/forloop2.java
@@ -0,0 +1,9 @@
+package com.ja.loops;
+
+public class forloop2 {
+    public static void main(String[] args) {
+        for(int i =1; i<=10; i++){
+            System.out.println(8 + "/" + i + "=" + (8/i));
+        }
+    }
+}

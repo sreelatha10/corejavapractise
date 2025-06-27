00a0a79d8e1e1a4234c122cc898b6aee613ac14e
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 13:51:09 +0530

    ifloop

---
+++ src/com/ja/conditions/ifexample.java
@@ -0,0 +1,10 @@
+package com.ja.conditions;
+
+public class ifexample {
+    public static void main(String[] args) {
+        int number = 9;
+        if (number > 7) {
+            System.out.println("number is greater");
+        }
+    }
+}

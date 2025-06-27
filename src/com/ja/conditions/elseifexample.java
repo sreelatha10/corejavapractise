1d71e90032da768941922de66dc73fa0778d06bf
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 13:59:01 +0530

    elseif

---
+++ src/com/ja/conditions/elseifexample.java
@@ -0,0 +1,14 @@
+package com.ja.conditions;
+
+public class elseifexample {
+    public static void main(String[] args) {
+        int number = 6;
+        if (number * 5 == 0){
+            System.out.println("the number is true");
+
+        } else {
+            System.out.println();
+            System.out.println("the number is false");
+        }
+    }
+}

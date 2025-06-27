11591c3db6d7572199aff480aebe14224c232d74
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 14:27:39 +0530

    ifelseif2

---
+++ src/com/ja/conditions/ifelseif2.java
@@ -0,0 +1,17 @@
+package com.ja.conditions;
+
+public class ifelseif2 {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num <= 8) {
+            System.out.println("num is less or equal to 8");
+        } else if (num <= 9) {
+            System.out.println("num is less or equal to 9");
+        } else if (num <= 0) {
+            System.out.println("num is less or equal to 0");
+        } else {
+            System.out.println("any num equal or not" +
+                    "");
+        }
+    }
+}

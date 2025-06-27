b237c5244cfc01d1396f7c5137c52e6e0e277fe3
sreelatha10 <sreelatharentachintala@gmail.com>
2025-06-27 14:11:35 +0530

    ifelseif

---
+++ src/com/ja/conditions/ifelseif.java
@@ -0,0 +1,21 @@
+package com.ja.conditions;
+
+public class ifelseif {
+    public static void main(String[] args) {
+
+        int even = 2;
+        if (even == 4) {
+            System.out.println("number is 4");
+
+        } else if (even == 2){
+            System.out.println("number is 2");
+
+        } else if (even == 6) {
+            System.out.println("number is 6");
+
+
+        } else {
+            System.out.println("true or false");
+        }
+    }
+}

9c139c01b8e93fe8eb50850a56c5580becf8ddd1
sreelatha10 <sreelatharentachintala@gmail.com>
2025-07-06 15:37:07 +0530

    inheritance

---
+++ src/com/ja/inheritance/inheritanceexample1.java
@@ -0,0 +1,27 @@
+package com.ja.inheritance;
+
+    class Animal {
+        void eat() {
+            System.out.println("Animal eats");
+        }
+    }
+    class Dog extends Animal {
+        void bark() {
+            System.out.println("Dog barks");
+        }
+    }
+    class Puppy extends Dog {
+        void weep() {
+            System.out.println("Puppy weeps");
+        }
+    }
+    public class inheritanceexample1 {
+        public static void main(String[] args) {
+            Puppy mypuppy = new Puppy();
+            mypuppy.eat();
+            mypuppy.bark();
+            mypuppy.weep();
+        }
+}
+
+

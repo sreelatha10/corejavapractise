157991be8bed7fbaeb42e9446332f643d0087b09
sreelatha10 <sreelatharentachintala@gmail.com>
2025-07-21 15:11:14 +0530

    set

--- src/com/ja/collections/List1.java
+++ src/com/ja/collections/List1.java
@@ -2,14 +2,25 @@ package com.ja.collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class List1 {
     public static void main(String[] args) {
         List<Integer> ab = new ArrayList<>();
-        ab.add(7);
-        ab.add(7);
-        ab.add(7);
-        ab.add(7);
+        ab.add(100);
+        ab.add(2);
+        ab.add(36);
+        ab.add(94);
+        ab.add(57);
+        ab.add(61);
         System.out.println(ab);
+        Set<Integer> cd = new HashSet<>();
+        cd.add(2);
+        cd.add(8);
+        cd.add(12);
+        cd.add(98);
+        cd.add(77);
+        System.out.println(cd);
     }
 }
